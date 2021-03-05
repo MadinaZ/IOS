c9ab564745a70e2e2ef110d9343c4037f6d0e9f7
Madina <madina2200@gmail.com>
2021-03-05 13:48:53 +0600

    UNIT-4

--- twitter_ios_starter/Twitter/HomeTableTableViewController.swift
+++ twitter_ios_starter/Twitter/HomeTableTableViewController.swift
@@ -25,6 +25,11 @@ class HomeTableTableViewController: UITableViewController {
         tableView.refreshControl = myRefreshControl
         
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+    }
 
     @objc func loadTweets(){
         
@@ -107,6 +112,10 @@ class HomeTableTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
         
         return cell
     }
